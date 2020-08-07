da702d219058300ada54cdef306593e4a8614f33
3xter <3x-ter@mail.ru>
2020-08-07 11:57:54 +0300

    Made the first difficult lesson

---
+++ app.js
@@ -0,0 +1,9 @@
+// 1)
+const num = 266219;
+// 2)
+const newNum = num.toString().split('').map(item => parseInt(item)).reduce((a, b) => a * b);
+console.log(newNum);
+// 3)
+const pow3 = newNum ** 3;
+console.log(pow3);
+console.log((pow3).toString().split('').map(item => parseInt(item)).slice(0, 2).join(''));
