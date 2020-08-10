2a13309e87e17a8dcf82919598f4ff90f1cc096c
3xter <3x-ter@mail.ru>
2020-08-10 12:17:16 +0300

    Did lesson03

--- app.js
+++ app.js
@@ -1,9 +1,44 @@
 // 1)
-const num = 266219;
+// a
+let lang = prompt('Выберете язык. en или ru')
+ if (lang === 'ru' || lang === 'en') {
+    if (lang === 'en') {
+        alert('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday')
+        console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday')
+    } else {
+        alert('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье')
+        console.log('Понедельник, вторник, среда, пятница, суббота, воскресенье')
+    }
+} else {
+    alert(`Нужно было быть внимательнее... Написано же en или ru, а ты ввел ${lang}`)
+}
+// b
+lang = prompt('Выберете язык. en или ru')
+if (lang === 'ru' || lang === 'en') {
+    switch (lang) {
+        case "en":
+            alert('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday')
+            console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday')
+            break
+        case "ru":
+            alert('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье')
+            console.log('Понедельник, вторник, среда, пятница, суббота, воскресенье')
+            break
+    }
+}else {
+    alert(`Нужно было быть внимательнее... Написано же en или ru, а ты ввел ${lang}`)
+}
+// c
+let weekday = {
+    ru: 'Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье',
+    en: 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday'
+}
+lang = prompt('Выберете язык. en или ru')
+
+alert(weekday[lang])
 // 2)
-const newNum = num.toString().split('').map(item => +item).reduce((a, b) => a * b);
-console.log(newNum);
-// 3)
-const pow3 = newNum ** 3;
-console.log(pow3);
-console.log((pow3).toString().split('').map(item => parseInt(item)).slice(0, 2).join(''));
+
+let namePerson = prompt('Введите имя')
+let message = (namePerson === 'Артем') ? 'Директор' :
+    (namePerson === 'Максим') ? 'Преподаватель' : 'Студент'
+console.log(message);
