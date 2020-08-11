68b02e5a148538707e129384a40d3e82572f2d26
3xter <3x-ter@mail.ru>
2020-08-11 12:19:41 +0300

    Ddi lesson04

--- app.js
+++ app.js
@@ -1,49 +1,10 @@
-// 1)
-// a
-let lang = prompt('Выберете язык. en или ru')
- if (lang === 'ru' || lang === 'en') {
-    if (lang === 'en') {
-        alert('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday')
-        console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday')
+function thirtyCharacters(str) {
+    if (typeof str === "string") {
+        let strNotSpace = str.trim()
+        if (str.length >= 30) {
+            console.log(strNotSpace.substr(0, 30) + '...');
+        } else { console.log(strNotSpace) }
     } else {
-        alert('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье')
-        console.log('Понедельник, вторник, среда, пятница, суббота, воскресенье')
+        alert('Это не строка')
     }
-} else {
-    alert(`Нужно было быть внимательнее... Написано же en или ru, а ты ввел ${lang}`)
 }
-// b
-lang = prompt('Выберете язык. en или ru')
-if (lang === 'ru' || lang === 'en') {
-    switch (lang) {
-        case "en":
-            alert('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday')
-            console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday')
-            break
-        case "ru":
-            alert('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье')
-            console.log('Понедельник, вторник, среда, пятница, суббота, воскресенье')
-            break
-    }
-}else {
-    alert(`Нужно было быть внимательнее... Написано же en или ru, а ты ввел ${lang}`)
-}
-// c
-let map = new Map([
-    ['ru', 'Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье'],
-    ['en', 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday']
-])
-
-lang = prompt('Выберете язык. en или ru')
-
-alert(map.get(lang))
-// 2)
-
-let namePerson = prompt('Введите имя')
-let message = (namePerson === 'Артем') ? 'Директор' :
-    (namePerson === 'Максим') ? 'Преподаватель' : 'Студент'
-console.log(message);
-
-
-
-
