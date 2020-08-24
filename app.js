49c59d7ac03e7e712918ad1f25a41bfbf909c70b
3xter <3x-ter@mail.ru>
2020-08-24 21:55:26 +0300

    did lesson13

--- app.js
+++ app.js
@@ -1,81 +1,17 @@
-
+let change = document.querySelector('#change')
+let wrapper = document.querySelector('.wrapper')
 let text = document.querySelector('.text')
-let miniText = document.querySelector('.mini-text')
 
-const printDate = function() {
-
-    let data = new Date();
-    let year = data.getFullYear();
-    let numberMonth = data.getMonth();
-    let numberWeekDay = data.getDay()
-    let day = data.getDate()
-    let numberHour = data.getHours();
-    let numberMinutes = data.getMinutes();
-    let seconds = data.getSeconds();
-    let month;
-    let weekDay;
-
-
-    switch (numberMonth)
-    {
-        case 0: month="января"; break;
-        case 1: month="февраля"; break;
-        case 2: month="марта"; break;
-        case 3: month="апреля"; break;
-        case 4: month="мае"; break;
-        case 5: month="июня"; break;
-        case 6: month="июля"; break;
-        case 7: month="августа"; break;
-        case 8: month="сентября"; break;
-        case 9: month="октября"; break;
-        case 10: month="ноября"; break;
-        case 11: month="декабря"; break;
+change.addEventListener('click', () => {
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
     }
 
-    switch (numberWeekDay) {
-        case 0: weekDay="Воскресенье"; break;
-        case 1: weekDay="Понедельник"; break;
-        case 2: weekDay="Вторник"; break;
-        case 3: weekDay="Среда"; break;
-        case 4: weekDay="Четверг"; break;
-        case 5: weekDay="Пятница"; break;
-        case 6: weekDay="Суббота"; break;
-    }
+    let randomNumber = getRandomInt(1, 1000000000).toString(16)
 
-    let hourWord = numberHour === 0 || numberHour >= 5 && numberHour <= 20 ? 'часов' : numberHour >=2 &&
-    numberHour <= 4 || numberHour === 22 || numberHour === 23 ? 'часа' : 'час';
+    text.textContent = `#${randomNumber}`
+    wrapper.style.backgroundColor = `#${randomNumber}`
 
-    let minutesWord = numberMinutes % 10 === 1 ? 'минута' : numberMinutes >= 5 && numberMinutes <=20 ||
-        numberMinutes >= 25 && numberMinutes <= 30 || numberMinutes >= 35 && numberMinutes <=40 ||
-        numberMinutes >= 45 && numberMinutes <=50 || numberMinutes >= 55 && numberMinutes <= 60 || numberMinutes === 0? 'минут' : 'минуты';
-
-    let secondsWord = seconds % 10 === 1 ? 'секунда' : seconds >= 5 && seconds <=20 ||
-    seconds >= 25 && seconds <= 30 || seconds >= 35 && seconds <=40 ||
-    seconds >= 45 && seconds <=50 || seconds >= 55 && seconds <= 60  || seconds === 0? 'секунд' : 'секунды';
-
-    // let li = document.createElement('li')
-    text.textContent = `Сегодня ${weekDay}, ${day} ${month} ${year} года, ${numberHour} ${hourWord} ${numberMinutes} ${minutesWord} ${seconds} ${secondsWord}`;
-    // list.appendChild(li);
-}
-
-setInterval(printDate, 1000);
-
-const addZero = n => n < 10 ? '0'+ n : n;
-
-const miniData = function () {
-
-    let data = new Date();
-    let year = data.getFullYear();
-    let numberMonth = data.getMonth() + 1;
-    let day = data.getDate()
-    let numberHour = data.getHours();
-    let numberMinutes = data.getMinutes();
-    let seconds = data.getSeconds();
-
-
-    miniText.textContent = `${addZero(numberHour)}:${addZero(numberMinutes)}:${addZero(seconds)}  ${addZero(numberMonth)}:${addZero(day)}:${addZero(year)}`;
-
-
-}
-
-setInterval(miniData, 1000)
+})
